a4cfca0836bfdaad27c18ff6fec6a2b9af63fd8c
nikola chakarov <chakarovn1975@gmail.com>
2022-11-28 10:21:24 +0200

    ...

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -2,8 +2,17 @@ import { LightningElement } from 'lwc';
 
 export default class HelloConditionalRendering extends LightningElement {
     isVisible = false;
+    name;
 
     handleClick() {
         this.isVisible = !this.isVisible;
+    }
+
+    handleChange(e) {
+        this.name = e.target.value;
+    }
+
+    get helloMessage() {
+        return this.name == 'hello';
     }
 }
