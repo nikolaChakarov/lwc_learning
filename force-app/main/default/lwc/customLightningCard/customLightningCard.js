3d4a84f42845a92d9c85870d96100d880b1ad7cc
nikola chakarov <chakarovn1975@gmail.com>
2022-12-02 11:26:43 +0200

    aura to lwc

--- force-app/main/default/lwc/customLightningCard/customLightningCard.js
+++ force-app/main/default/lwc/customLightningCard/customLightningCard.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class CustomLightningCard extends LightningElement {}
+export default class CustomLightningCard extends LightningElement {
+
+    handleSlotFooterChange(e) {
+        const footerEl = this.template.querySelector('footer');
+
+        if (footerEl) {
+            footerEl.classList.remove('slds-hide');
+        }
+    }
+
+}
